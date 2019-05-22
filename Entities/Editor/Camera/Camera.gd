fa4dd513ecfba218f866d9778c765472ba53018d
Ruixel <bag4life5@gmail.com>
2019-05-22 21:56:37 +0100

    Camera zoom, drag and rotation implementation

---
+++ Entities/Editor/Camera/Camera.gd
@@ -0,0 +1,64 @@
+extends Spatial
+
+const CAM_ZOOM_MIN = 3
+const CAM_ZOOM_MAX = 10
+
+const CAM_ANGLE_MIN = 15.0
+const CAM_ANGLE_MAX = 85.0
+
+const CAM_DRAG_SPEED = 0.5
+
+var camPosition = Vector2(0, 0)
+var camAngle = 35.0
+var camZoom = 6
+
+var mouse_motion = Vector2()
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("editor_camera_pan"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+		# Drag across level relative to camera angle
+		var ang = self.rotation.y
+		var y_speed = mouse_motion.y * delta
+		var x_speed = mouse_motion.x * delta
+		camPosition.x += CAM_DRAG_SPEED * ((cos(self.rotation.y) * y_speed)  + (sin(self.rotation.y) * -x_speed))
+		camPosition.y += CAM_DRAG_SPEED * ((sin(self.rotation.y) * -y_speed) + (cos(self.rotation.y) * -x_speed))
+		
+	elif Input.is_action_pressed("editor_camera_rotate"):
+		# Rotate up/down
+		camAngle += mouse_motion.y * delta * 10
+		camAngle  = clamp(camAngle, 15.0, 85.0)
+		
+		# Rotate side to side
+		self.rotate_y(-mouse_motion.x * delta * 0.2)
+	else:
+		# Show mouse if doing nothing
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	# Move the camera away from the target (zoom distance)
+	$Camera.transform.origin.y = sin(deg2rad(camAngle)) * camZoom
+	$Camera.transform.origin.x = -cos(deg2rad(camAngle)) * camZoom
+	$Camera.rotation.x         = -deg2rad(camAngle)
+	
+	# Lerp camera to match grid position
+	var finalPos = Vector3(camPosition.x, 0, camPosition.y)
+	self.global_transform.origin = lerp(self.global_transform.origin, finalPos, 0.5)
+	
+	# Reset mouse movement
+	mouse_motion = Vector2(0, 0)
+
+
+func _input(event: InputEvent) -> void:
+	# Mouse movement
+	if event is InputEventMouseMotion:
+		mouse_motion = event.relative
+
+	# Mouse scroll
+	if event.is_action_pressed("editor_camera_zoom_in"):
+		camZoom -= 1
+		camZoom = clamp(camZoom, CAM_ZOOM_MIN, CAM_ZOOM_MAX)
+	if event.is_action_pressed("editor_camera_zoom_out"):
+		camZoom += 1
+		camZoom = clamp(camZoom, CAM_ZOOM_MIN, CAM_ZOOM_MAX)
--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -1,6 +1,6 @@
 extends MeshInstance
 
-func generate_grid_mesh(num_x, num_y, space_x, space_y):
+func generate_grid_mesh(num_x: int, num_y: int, space_x: float, space_y: float) -> SurfaceTool:
 	var surface_tool = SurfaceTool.new()
 	
 	surface_tool.begin(Mesh.PRIMITIVE_LINES)
@@ -8,10 +8,10 @@ func generate_grid_mesh(num_x, num_y, space_x, space_y):
 	var index = 0
 	for x in range(0, num_x +1):
 		surface_tool.add_color(Color(1, 0, 0, 1))
-		surface_tool.add_vertex(Vector3((num_x/2)*space_x, 0, (x - num_x/2)*space_x))
+		surface_tool.add_vertex(Vector3(num_x * space_x, 0, x * space_x))
 		
 		surface_tool.add_color(Color(1, 0, 0, 1));
-		surface_tool.add_vertex(Vector3(-(num_x/2)*space_x, 0, (x - num_x/2)*space_x))
+		surface_tool.add_vertex(Vector3(0, 0, x * space_x))
 
 		surface_tool.add_index(index);
 		surface_tool.add_index(index+1);
@@ -20,10 +20,10 @@ func generate_grid_mesh(num_x, num_y, space_x, space_y):
 	
 	for y in range(0, num_y + 1):
 		surface_tool.add_color(Color(1, 0, 0, 1))
-		surface_tool.add_vertex(Vector3((y - num_y/2)*space_x, 0, (num_y/2)*space_y))
+		surface_tool.add_vertex(Vector3(y * space_x, 0, num_y * space_y))
 		
 		surface_tool.add_color(Color(1, 0, 0, 1));
-		surface_tool.add_vertex(Vector3((y - num_y/2)*space_x, 0, -(num_y/2)*space_y))
+		surface_tool.add_vertex(Vector3(y * space_y, 0, 0))
 
 		surface_tool.add_index(index);
 		surface_tool.add_index(index+1);
