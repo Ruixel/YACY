b570e090f5e614181c9460301242ae3a159b6de8
Ruixel <bag4life5@gmail.com>
2019-05-25 23:45:21 +0100

    Baisc wall creation

--- scripts/3DCursor.gd
+++ scripts/3DCursor.gd
@@ -9,6 +9,11 @@ var grid_spacing = 1
 var grid_num = 20
 var grid_pos = Vector2()
 
+var placement_start = Vector2()
+var placement_end = Vector2()
+
+var wall
+
 func _process(delta: float) -> void:
 	if mouse_motion != Vector2():
 		# Cast ray directly from camera
@@ -24,6 +29,21 @@ func _process(delta: float) -> void:
 			
 			self.transform.origin = Vector3(grid_pos.x * grid_spacing, 0, grid_pos.y * grid_spacing)
 	
+	if Input.is_action_just_pressed("editor_place"):
+		placement_start = grid_pos
+		
+		wall = MeshInstance.new()
+		get_node("../World").add_child(wall)
+	
+	if Input.is_action_pressed("editor_place"):
+		placement_end = grid_pos
+		
+		if placement_start != placement_end:
+			buildWall(wall)
+	
+	if Input.is_action_just_released("editor_place"):
+		pass
+	
 	mouse_motion = Vector2()
 
 func _input(event: InputEvent) -> void:
@@ -31,3 +51,39 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		mouse_motion = event.relative
 
+var quad_indices = [0, 1, 3, 1, 2, 3] # Magic array 
+func createQuadMesh(surface_tool : SurfaceTool, wall_vertices : Array, sIndex: int) -> void:
+	# Add Vertices
+	surface_tool.add_vertex(wall_vertices[0])
+	surface_tool.add_vertex(wall_vertices[3])
+	surface_tool.add_vertex(wall_vertices[2])
+	surface_tool.add_vertex(wall_vertices[1])
+	
+	# Calculate Normal
+	var normal = (wall_vertices[2] - wall_vertices[1]).cross(wall_vertices[3] - wall_vertices[1])
+	for x in range(1, 4):
+		surface_tool.add_normal(normal)
+	
+	# Quad Indices
+	for idx in quad_indices:
+		surface_tool.add_index(sIndex + idx)
+
+func buildWall(meshRef : MeshInstance) -> void:
+	var surface_tool = SurfaceTool.new()
+	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+	var minHeight = 0
+	var maxHeight = 2
+	
+	# Calculate wall vertices
+	var wall_vertices = []
+	wall_vertices.insert(0, Vector3(placement_start.x, minHeight, placement_start.y))
+	wall_vertices.insert(1, Vector3(placement_start.x,  maxHeight, placement_start.y))
+	wall_vertices.insert(2, Vector3(placement_end.x, maxHeight, placement_end.y))
+	wall_vertices.insert(3, Vector3(placement_end.x, minHeight, placement_end.y))
+	createQuadMesh(surface_tool, wall_vertices, 0)
+	
+	var bVertices = [wall_vertices[3], wall_vertices[2], wall_vertices[1], wall_vertices[0]]
+	createQuadMesh(surface_tool, bVertices, 4)
+	
+	meshRef.mesh = surface_tool.commit()
