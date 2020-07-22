b39a3c8e82991ed355b8e4ebbb9400aa124152f7
Ruixel <bag4life5@gmail.com>
2020-07-22 01:20:37 +0100

    Load levels and screenshots via GraphQL

--- scripts/gui/LevelList.gd
+++ scripts/gui/LevelList.gd
@@ -10,113 +10,62 @@ var data = [
 		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze250SS.jpg",
 		"legacy": true,
 		"gameNumber": 250
-	},
-	{
-		"name": "The Castle",
-		"author": "ChallengeYou",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze253SS.jpg",
-		"legacy": true,
-		"gameNumber": 253,
-	},
-	{
-		"name": "Pacmania",
-		"author": "ChallengeYou",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze255SS.jpg",
-		"legacy": true,
-		"gameNumber": 255,
-	},
-	{
-		"name": "Trash Game",
-		"author": "Xard",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/DefaultSS.jpg",
-		"legacy": true,
-		"gameNumber": 54323,
-	},
-	{
-		"name": "Cringe Story",
-		"author": "DaVinci",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/DefaultSS.jpg",
-		"legacy": true,
-		"gameNumber": 43121,
-	},
-	{
-		"name": "ChallengeYou HQ",
-		"author": "NotPacman94",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze65604SS.jpg",
-		"legacy": true,
-		"gameNumber": 65604,
-	},
-	{
-		"name": "Good Night",
-		"author": "Haakson",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze65545SS.jpg",
-		"legacy": true,
-		"gameNumber": 65545,
-	},
-	{
-		"name": "ChallengeYou HQ",
-		"author": "NotPacman94",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze65604SS.jpg",
-		"legacy": true,
-		"gameNumber": 65604,
-	},
-	{
-		"name": "Good Night",
-		"author": "Haakson",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze65545SS.jpg",
-		"legacy": true,
-		"gameNumber": 65545,
-	},
-	{
-		"name": "ChallengeYou HQ",
-		"author": "NotPacman94",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze65604SS.jpg",
-		"legacy": true,
-		"gameNumber": 65604,
-	},
-	{
-		"name": "Good Night",
-		"author": "Haakson",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze65545SS.jpg",
-		"legacy": true,
-		"gameNumber": 65545,
-	},
-	{
-		"name": "ChallengeYou HQ",
-		"author": "NotPacman94",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze65604SS.jpg",
-		"legacy": true,
-		"gameNumber": 65604,
-	},
-	{
-		"name": "Good Night",
-		"author": "Haakson",
-		"screenshot": "res://res/txrs/gui/sample_screenshots/Maze65545SS.jpg",
-		"legacy": true,
-		"gameNumber": 65545,
-	},
+	}
 ]
 
 func _ready():
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
+	var query = '{"query": "{ latestLevels(page: 4, pageSize: 25) { title, author, creationDate, plays, screenshot}}"}'
+	var headers : PoolStringArray
+	headers.append("Content-Type: application/json")
+	
+	$HTTPRequest.request("http://localhost:4000/graphql", headers, true, HTTPClient.METHOD_POST, query)
+
+func _on_request_completed(result, response_code, headers, body):
+	var response = body.get_string_from_utf8()
+	#print(response)
+	
+	var r = JSON.parse(response).result
+	data = r.data.latestLevels
 	loadLevels()
 
 func loadLevels():
 	for item in data:
-		var img = Image.new()
-		if (img.load(item.screenshot) != 0):
-			push_warning("Warning: Could not load " + item.screenshot)
-			return
-			
-		var new_tex = ImageTexture.new()
-		new_tex.create_from_image(img)
-		
 		var new_lvl = Level.instance()
-		new_lvl.texture_normal = new_tex
-		new_lvl.get_node("Title").text = item.name
+		new_lvl.get_node("Title").text = item.title
 		new_lvl.get_node("Author").text = item.author
-		if item.legacy:
-			new_lvl.get_node("Archived").visible = true
+#		if item.legacy:
+#			new_lvl.get_node("Archived").visible = true
 		
+		if (item.screenshot == "/Screenshots/DefaultSS.jpg"):
+			var img = Image.new()
+			if (img.load("res://res/txrs/gui/DefaultSS.jpg") != 0):
+				push_warning("Warning: Could not load " + item.screenshot)
+				return
+
+			var new_tex = ImageTexture.new()
+			new_tex.create_from_image(img)
+
+			new_lvl.texture_normal = new_tex
+		else:
+			var img_request = HTTPRequest.new()
+			add_child(img_request)
+			img_request.connect("request_completed", self, "_img_request_completed", [new_lvl, item.screenshot])
+			img_request.request("http://localhost:4000" + item.screenshot)
 		
 		add_child(new_lvl)
 
+func _img_request_completed(result, response_code, headers, body, new_lvl, screenshot):
+	var img = Image.new()
+	var error = img.load_jpg_from_buffer(body)
+	if error != OK:
+		print(error)
+		print("Couldn't load the screenshot at " + screenshot)
+		return
+		
+	var new_tex = ImageTexture.new()
+	new_tex.create_from_image(img)
+	
+	new_lvl.texture_normal = new_tex
+	
+	
