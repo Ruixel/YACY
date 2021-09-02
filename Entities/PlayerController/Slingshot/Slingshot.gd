9bbf27b41339df7721263d873cb76c189df12919
Adrian <13946497+Ruixel@users.noreply.github.com>
2021-09-03 00:32:18 +0100

    Pullback slingshot animation

--- Entities/PlayerController/Slingshot/Slingshot.gd
+++ Entities/PlayerController/Slingshot/Slingshot.gd
@@ -2,11 +2,13 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _unhandled_input(event):
 	if event.is_action_pressed("shoot"):
 		print("shoot")
+		$AnimationPlayer.playback_speed = 0.8
 		$AnimationPlayer.play("ArmatureAction")
 	if event.is_action_released("shoot"):
-		$AnimationPlayer.stop()
+		$AnimationPlayer.playback_speed = 5
+		$AnimationPlayer.play_backwards("ArmatureAction")
