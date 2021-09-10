429b23717bf159de6dc22ef3c093c6f47e0b0634
Adrian <13946497+Ruixel@users.noreply.github.com>
2021-09-10 22:11:21 +0100

    Mimick the erratic chaser movement

--- Entities/Legacy/Chasers/Chaser.gd
+++ Entities/Legacy/Chasers/Chaser.gd
@@ -2,7 +2,8 @@ extends KinematicBody
 
 var player_inside = null
 var active := false
-var speed = 100
+var max_speed = 4
+var speed = 4
 
 var id = null
 var model = null
@@ -13,9 +14,8 @@ const models := {
 }
 
 func _physics_process(delta):
-	if player_inside:
-		target_player()
-		move_and_slide_with_snap(global_transform.basis.z * Vector3(1, 0, 1) * delta * speed, Vector3(0, -0.1, 0), Vector3(0, 1, 0))
+	if active and player_inside:
+		move_and_slide_with_snap(global_transform.basis.z * Vector3(1, 0, 1) * speed, Vector3(0, -0.1, 0), Vector3(0, 1, 0))
 
 func set_material_opacity(opacity: float):
 	if model != null:
@@ -46,15 +46,24 @@ func set_model(model_id):
 		mesh.mesh.surface_set_material(i, surface)
 
 func _on_NearArea_body_entered(body):
-	if body.has_meta("player"):
+	if body.has_meta("player") and player_inside == null:
 		self.player_inside = body
 		target_player()
 		active = true
+		$Timer.start()
 		
 		if id == 3:
 			set_material_opacity(0.6)
 		else: 
 			set_material_opacity(1.0)
+
+func _on_FarArea_body_exited(body):
+	if body.has_meta("player") and self.player_inside == body:
+		self.player_inside = null
+		active = false
+		$Timer.stop()
+		
+		set_material_opacity(0.175)
 
 func target_player():
 	if player_inside != null:
@@ -72,6 +81,15 @@ func target_player():
 		
 		# Don't turn if chaser is directly infront of player (or nasty errors)
 		if abs(angle) < 0.01 or abs(dot) > 0.99:
+#			var plane = Plane(dir_to_player, self_transform.distance_to(Vector3(0,0,0)))
+#			if not plane.is_point_over(player_transform):
+#				rotate_y(deg2rad(180.0))
+			var test = self_transform + (dir * 0.2)
+			if self_transform.distance_to(player_transform) < test.distance_to(player_transform):
+				rotate_y(deg2rad(180.0))
+#			if dir.cross(Vector3(0, 1, 0)).dot(dir_to_player) < 0:
+#				rotate_y(deg2rad(180.0))
+			#print(dir_to_player.angle_to())
 			return
 
 		# Figure if the player is to the left or right to the chaser
@@ -79,3 +97,6 @@ func target_player():
 			rotate_y(-angle)
 		else:
 			rotate_y(angle)
+
+func _on_Timer_timeout():
+	target_player()
