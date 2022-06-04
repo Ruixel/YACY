58a7bf066698c0b3dc5d6968874dc5574d22b7c1
Ruixel <13946497+Ruixel@users.noreply.github.com>
2022-06-04 23:11:10 +0100

    Add selected item UI prototype

---
+++ scripts/gui/PolygonRotate.gd
@@ -0,0 +1,15 @@
+extends Polygon2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#for p in polygon:
+	#	p *= delta * 5
+	#rotate(delta)
+	pass
---
+++ scripts/gui/WaveEffect.gd
@@ -0,0 +1,11 @@
+extends Spatial
+
+export var wave_speed: float
+
+var p = PI/2
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	p = p + (delta*wave_speed)
+	translate(Vector3(0, sin(p)*0.0003, 0))
+	$Cube.rotate_y(sin(p)*0.004)
